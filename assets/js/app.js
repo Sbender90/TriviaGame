be98a77d08bacfc03177dee703baa1bb6fb09249
Steven Bender <sbender1990@gmail.com>
2018-02-15 00:16:57 -0600

    second

---
+++ assets/js/app.js
@@ -0,0 +1,38 @@
+var triviaQuestions = [{
+	question: "In what year was Pixar founded?",
+	answerList: ["1979", "1986", "1995", "2000"],
+	answer: 1
+},{
+	question: "Which tech mogul provided funding and became a co-founder of Pixar?",
+	answerList: ["Steve Jobs", "Bill Gates", "Peter Thiel", "Mark Zuckerberg"],
+	answer: 0
+},{
+	question: "What was Pixar's first feature-length film that was released in 1995?",
+	answerList: ["Toy Story", "A Bug's Life", "Monster's Inc", "Finding Nemo"],
+	answer: 0
+},{
+	question: "Who was the first Pixar character added to the Disney Princess line-up?",
+	answerList: ["Jessie", "Repunzel", "Merida", "Elsa"],
+	answer: 2
+},{
+	question: "What's the name of Pixar's first short film, also known as their mascot?",
+	answerList: ["Lampo", "Junior", "Pixie", "Luxo Jr."],
+	answer: 3
+},{
+	question: "How many sequels does Pixar currently have released? (as of August 2016)",
+	answerList: ["5", "3", "6", "7"],
+	answer: 0
+},{
+	question: "Which film won Pixar's first Academy Award for Best Animated Feature?",
+	answerList: ["Toy Story", "Finding Nemo", "Up", "Wall-E"],
+	answer: 1
+},{
+	question: "Who directed Pixar's first three feature films?",
+	answerList: ["Peter Docter", "Brad Bird", "John Lasseter", "Peter Sohn"],
+	answer: 2
+},{
+	question: "Who voiced Sadness in 'Inside Out'?",
+	answerList: ["Amy Poehler", "Phyllis Smith", "Mindy Kaling", "Phyllis Vance"],
+	answer: 1
+}];
+
