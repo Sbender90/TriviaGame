56536a113998071f4976938d5d8cb86f5bde90a6
Steven Bender <sbender1990@gmail.com>
2018-02-15 23:41:00 -0600

    js

--- assets/js/app.js
+++ assets/js/app.js
@@ -36,3 +36,55 @@ var triviaQuestions = [{
 	answer: 2
 }];
 
+var liveQuestion;
+var rightAnswers;
+var incorrectAnswers;
+var unansweredQuestions;
+var userSelect;
+var time;
+var answered;
+var seconds;
+
+var answerMessage = {
+	correct: "Correct on to the next one!",
+	incorrect: "That's not right, lets move on.",
+	timesUp: "You are out of time.",
+	finished: "Let's see how you did!"
+
+}
+
+$('#startBtn').on('click', function () {
+	$(this).hide();
+	$("#my-4").hide();
+	newGame();
+});
+
+function newGame() {
+	$('#finalMessage').empty();
+	$('#rightAnswer').empty();
+	$('#wrongAnswers').empty();
+	$('unansweredQuestions').empty();
+	liveQuestion = 0;
+	rightAnswers = 0;
+	wrongAnswers = 0;
+	unansweredQuestions = 0;
+	generateQuestion ();
+}
+
+function generateQuestion () {
+	$('#gameFeedBack').empty();
+	$('rightAnswer').empty();
+	answered = true;
+
+	$('#liveQuestion').html((liveQuestion+1) + '/' + triviaQuestions.length);
+	$('#question').html("<h2> + triviaQuestions[liveQuestion].question + '</h2>");
+
+	for(i = 0; i < answerList.length; i ++) {
+		var choiceList = $('<div>');
+		choices.text(triviaQuestions[liveQuestion].answerList[i]);
+		choices.attr ({"data-index": i });
+		choices.addClass('userChoice');
+		$('.answerList').append(choices);
+	}
+}
+console.log(generateQuestion);
